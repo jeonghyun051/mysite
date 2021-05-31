40a4bd0b88ba04486c625b39bb88df71feb18072
jeonghyun051 <rlawjdgus051@naver.com>
2021-05-31 10:52:59 +0900

    Servlet Life Circle & Listener

--- mysite02/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -16,6 +16,13 @@ public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
+	@Override
+	public void init() throws ServletException {
+		String configPath = getServletConfig().getInitParameter("config");
+		System.out.println("MainController.init() called:"+configPath);
+		super.init();
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String actionName = request.getParameter("a");
