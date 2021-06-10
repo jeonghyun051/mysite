2f7ce0059223d8fc80b9b0ca580f3c2283125174
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-10 11:15:27 +0900

    MessageConvert 적용, script jquery

--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.douzone.mysite.service.AdminService;
 import com.douzone.mysite.vo.SiteVo;
+import com.douzone.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
@@ -25,5 +27,22 @@ public class MainController {
 		model.addAttribute("vo",vo);
 		application.setAttribute("title", vo.getTitle());
 		return "main/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg1")
+	public String message1() {
+		return "안녕";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg2")
+	public UserVo message2() {
+		
+		UserVo vo = new UserVo();
+		vo.setNo(10L);
+		vo.setEmail("rlawjdgus@naver.com");
+		vo.setName("김정현00");		
+		return vo;
 	}
 }
